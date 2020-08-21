12bdbdc469f8f2549134d23ea0cc4fad9ebd28d2
joooohee <joooohee@kakao.com>
2020-08-21 10:55:54 +0900

    [ADD] Setting Socket.IO

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 import express from "express";
 import socketIO from "socket.io";
+import logger from "morgan";
 import { join } from "path";
 
 const PORT = 4000;
 const app = express();
 app.set("view engine", "pug");
 app.set("views", join(__dirname, "views"));
+app.use(logger("dev"));
 app.use(express.static(join(__dirname, "static")));
 app.get("/", (req, res) => res.render("home"));
 
@@ -13,4 +15,8 @@ const handleListening = () => console.log(`✅ Server is starting`);
 
 const server = app.listen(PORT, handleListening);
 
-const io = socketIO(server);
+const io = socketIO.listen(server);
+
+io.on("connection", (socket) => {
+  setTimeout(() => socket.broadcast.emit("hello"), 5000);
+});
