05bebc7c55831d529ab28003baa5def5ef59cc3c
joooohee <joooohee@kakao.com>
2020-10-31 20:48:58 +0900

    Add player board

--- assets/js/sockets.js
+++ assets/js/sockets.js
@@ -1,6 +1,7 @@
 import { handleNewMseaage } from "./chat";
 import { handleDisconnected, handleNewUser } from "./notifications";
 import { handleBeganPath, handleFilled, handleStrokedPath } from "./paint";
+import { handlePlayerUpdate } from "./players";
 
 let socket = null;
 
@@ -18,4 +19,5 @@ export const initSockets = (aSocket) => {
   socket.on(events.beganPath, handleBeganPath);
   socket.on(events.strokedPath, handleStrokedPath);
   socket.on(events.filled, handleFilled);
+  socket.on(events.playerUpdate, handlePlayerUpdate);
 };
